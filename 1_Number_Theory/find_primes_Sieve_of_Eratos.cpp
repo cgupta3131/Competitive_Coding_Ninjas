0f6c7654d77959fb9c13b0643aaa6f78dfa19a73
cgupta3131 <cgupta3131@gmail.com>
2018-09-24 20:15:55 +0530

    Number Theory-Part1

---
+++ 1_Number_Theory/find_primes_Sieve_of_Eratos.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	bool *arr = new bool[n+1];
+
+	for(int i=0;i<=n;i++)
+		arr[i] = true;
+
+	arr[0] = false;
+	arr[0] = false;
+
+
+	int x = int( sqrt(n) );
+
+
+	for(int i=2;i<=x;i++)
+	{
+		if(arr[i] == false)
+			continue;
+		else
+			for(int j=i*i ; j<=n ; j += i)
+				arr[j] = false;
+	}
+
+	int count = 0;
+	for(int i=2;i<=n;i++)
+		if(arr[i] == true)
+			count++;
+
+	cout << count << endl;
+
+
+	return 0 ; 
+
+}
+
